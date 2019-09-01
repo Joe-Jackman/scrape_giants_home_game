5d98744cba46e2a802e443cc5019ea1f08597b06
joe-jackman <iorihirado@gmail.com>
2019-09-01 21:39:49 +0900

    feat: ホームゲームの日本時間を取得

---
+++ main.py
@@ -0,0 +1,33 @@
+# coding: UTF-8
+from bs4 import BeautifulSoup
+import urllib.request, urllib.error
+import re
+import datetime
+
+# アクセスするURL(パラメタなしなら当月のGiantsスケジュールが帰ってくる)
+url = "https://www.giants.jp/smartphone/G/schedule/"
+
+# URLにアクセス
+html = urllib.request.urlopen(url=url)
+
+soup = BeautifulSoup(html, "lxml")
+tr_tags = soup.find_all("tr")
+
+date_pattern = '[0-9]+'
+time_pattern = '[0-9]+:[0-9]+'
+
+for tr in tr_tags:
+  if not tr.find('span', class_="home-game giants"): 
+    continue
+
+  time_result = re.search(time_pattern, tr.td.text)
+  date_result = re.search(date_pattern, tr.th.span.text)
+
+  now = datetime.datetime.now()
+  event_date = datetime.datetime.strptime(
+    str(now.year) + '-' + str(now.month) + '-' + date_result.group(0) + ' ' + time_result.group(0) + ':' + '00',
+    '%Y-%m-%d %H:%M:%S'
+    )
+
+  print(event_date)
+
