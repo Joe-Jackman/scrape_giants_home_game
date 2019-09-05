22271a14e0caae60fbaaf33ae6e4054c99f50103
joe-jackman <iorihirado@gmail.com>
2019-09-05 22:53:46 +0900

    feat: 試合開始日が過ぎた日付のエラー処理を追加

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ date_pattern = '[0-9]+'
 time_pattern = '[0-9]+:[0-9]+'
 
 send_to_slack = []
-slack = slackweb.Slack(url="https://hooks.slack.com/services/トークン")
+slack = slackweb.Slack(url="https://hooks.slack.com/services/とーくん")
 
 for tr in tr_tags:
   if not tr.find('span', class_="home-game giants"): 
@@ -26,11 +26,17 @@ for tr in tr_tags:
 
   time_result = re.search(time_pattern, tr.td.text)
   date_result = re.search(date_pattern, tr.th.span.text)
+  
+  # 試合日がすぎた日付は開始時間が消えるので正規表現に合致しなくなる
+  if not isinstance(time_result, re.Match) or not isinstance(date_result, re.Match):
+    continue
 
   now = datetime.datetime.now()
-  # event_date = datetime.datetime.strptime(
-  #   str(now.year) + '-' + str(now.month) + '-' + date_result.group(0) + ' ' + time_result.group(0) + ':' + '00',
-  #   '%Y-%m-%d %H:%M:%S'
-  #   )
-  send_to_slack.append(str(now.year) + '-' + str(now.month) + '-' + date_result.group(0) + ' ' + time_result.group(0) + ':' + '00')
-  slack.notify(text=str(now.year) + '-' + str(now.month) + '-' + date_result.group(0) + ' ' + time_result.group(0) + ':' + '00')
+  event_date = datetime.datetime.strptime(
+    str(now.year) + '-' + str(now.month) + '-' + date_result.group(0) + ' ' + time_result.group(0) + ':00',
+    '%Y-%m-%d %H:%M:%S'
+  )
+  send_to_slack.append(event_date)
+
+# print(send_to_slack)
+slack.notify()
